6d719079ad409c8399a1880ea3126b2f04b96a30
MarianaU <mariana.urreav@uqvirtual.edu.co>
2023-11-09 09:06:51 -0500

    login

--- src/app/pagina/pagina/login/login.component.ts
+++ src/app/pagina/pagina/login/login.component.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
+import { Alerta } from 'src/app/modelo/alerta';
 import { LoginDTO } from 'src/app/modelo/login-dto';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
 
-  loginDTO: LoginDTO;
 
-  constructor(){
-    
+  loginDTO: LoginDTO;
+  alerta!: Alerta;
+
+  constructor(private authService: AuthService, private tokenService: TokenService) {
+
     this.loginDTO = new LoginDTO();
   }
 
 
-  public logear(){
-    alert("Se inicio la peticion de registro");
-  }
-  
+  public login() {
+    this.authService.login(this.loginDTO).subscribe({
+      next: data => {
+        this.tokenService.login(data.respuesta.token);
+      },
+      error: error => {
+        this.alerta = { mensaje: error.error.respuesta, tipo: "danger" };
+      }
+    });
+  }
+
 }
